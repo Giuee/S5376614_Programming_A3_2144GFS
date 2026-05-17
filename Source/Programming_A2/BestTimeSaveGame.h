a99961e787080fdded1b87565c3086c20d309d7f
Giuee <eladio121@gmail.com>
2026-05-17 15:51:13 +1000

    Update
    focus on simon says

---
+++ Source/Programming_A2/BestTimeSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "BestTimeSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGRAMMING_A2_API UBestTimeSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
